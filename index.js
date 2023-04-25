6444aaadf02bcf61130da4dacc8003ca366548df
thanap151255 <thanap151255@gmail.com>
2023-04-25 14:08:12 +0700

    value

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 console.log('hello world!');
+// 
 
 let x = 2; 
 
@@ -7,3 +8,17 @@ let y = 10;
 let z = x + y;
 
 console.log(z);
+
+// 
+
+let myNumber = 5.5;
+
+let mySting = 'hi';
+
+let myBoolean = true;
+
+console.log(myNumber);
+console.log(mySting);
+console.log(myBoolean);
+
+// 
