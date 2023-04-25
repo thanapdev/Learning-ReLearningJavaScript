dbb9892ac64f8f064640f15642b6d25ab91f0de4
thanap151255 <thanap151255@gmail.com>
2023-04-25 14:23:15 +0700

    conditions

--- index.js
+++ index.js
@@ -22,3 +22,15 @@ console.log(mySting);
 console.log(myBoolean);
 
 // 
+
+let score = 70;
+
+if (score >= 80) {
+    console.log('you get A');
+}else if (score >= 70) {
+    console.log('you get B ');
+}else{
+   console.log('you lose');
+}
+
+// 
